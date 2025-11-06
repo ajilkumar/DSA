d3a589b072869b5862081a84e433c0bf0f25c6ab
ajilkumar <ajilk0325@gmail.com>
2025-11-06 14:03:19 +0530

    reverse triangle

--- patterns/triangle_pattern_02.js
+++ patterns/triangle_pattern_02.js
@@ -6,3 +6,29 @@ for (let i = 1; i <= 4; i++) {
   }
   console.log(row);
 }
+
+// output:
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+// ------------------------------- //
+
+// Reverse trinagle pattern
+{
+  let n = 4;
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = i + 1; j > 0; j--) {
+      row += j + " ";
+    }
+    console.log(row);
+  }
+}
+
+// output:
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
