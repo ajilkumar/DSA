f657f0390fc93f5d3cda9a3e6ca75ccaa1a503b3
ajilkumar <ajilk0325@gmail.com>
2025-11-21 19:17:17 +0530

    backtracking

---
+++ recursions/subarray.js
@@ -0,0 +1,21 @@
+// Given an integer array nums, return all possible subsets
+
+function subsets(nums) {
+  let result = [];
+  let temp = [];
+
+  function recursiveSubsets(nums, i) {
+    if (i === nums.length) {
+      return result.push([...temp]);
+    }
+    temp.push(nums[i]);
+    recursiveSubsets(nums, i + 1);
+    temp.pop();
+    recursiveSubsets(nums, i + 1);
+  }
+
+  recursiveSubsets(nums, 0);
+  return result;
+}
+
+console.log(subsets([1, 2, 3]));
