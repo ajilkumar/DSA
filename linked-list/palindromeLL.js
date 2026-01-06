02591b768fca2c361c04d56a3bc415d06fff4807
ajilkumar <ajilk0325@gmail.com>
2026-01-07 00:36:38 +0530

    palindrome linked list

---
+++ linked-list/palindromeLL.js
@@ -0,0 +1,47 @@
+/**
+ * 
+Palindrome Linked List
+----------------------------------------------------------------------
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+
+----------------------------------------------------------------------
+Example 1:
+
+Input: head = [1,2,2,1]
+Output: true
+----------------------------------------------------------------------
+Example 2:
+
+Input: head = [1,2]
+Output: false
+----------------------------------------------------------------------
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  let string1 = (string2 = "");
+  let node = head;
+
+  while (node != null) {
+    string1 = `${string1}${node.val}`;
+    string2 = `${node.val}${string2}`;
+    node = node.next;
+  }
+  return string1 === string2;
+};
